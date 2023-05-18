fa1d011080638c627b454b81df517bd8bc520b9f
lpujadas <lpujadas@gmail.com>
2023-05-18 10:54:48 -0300

    Update encrypted.go
    Elimine la funcion main. Recomendación del chatGPT 18/05/2023

--- encrypted.go
+++ encrypted.go
@@ -4,13 +4,3 @@ import (
 	"encoding/base64"
 	"fmt"
 )
-
-func main() {
-
-	var password string
-	fmt.Println("please enter password:")
-	fmt.Scanln(&password)
-	enc_password := base64.URLEncoding.EncodeToString([]byte(password))
-	fmt.Println("Encrypted password:" + enc_password)
-
-}
